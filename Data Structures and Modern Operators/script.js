cb67e3176de0a0533d5f0c61022ef0f1788aef2f
pavan <pavangurram56@gmail.com>
2021-12-21 11:08:35 +0530

    new folder added (DSA )

---
+++ Data Structures and Modern Operators/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+// let [main, second] = restaurant.categories;
+// console.log(main, second);
+
+// [main, second] = [second, main];
+// console.log(main, second);
+
+console.log(restaurant.order(2, 0));
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//nested destructuring
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//default values >>>it is useful when we get data from api
+const [p = 3, q, r = 4] = [, , , 8];
+console.log(p, q, r);
