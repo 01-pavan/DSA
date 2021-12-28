d1b5db2b2b22007b389e9e6aa68920013a1aa838
pavan <pavangurram56@gmail.com>
2021-12-28 23:18:51 +0530

    Strings.js file added

---
+++ Data Structures and Modern Operators/Strings.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const airline = 'Tap Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+
+console.log('B737'[0]); //prints the letter at the index 0
+
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+
+console.log();
