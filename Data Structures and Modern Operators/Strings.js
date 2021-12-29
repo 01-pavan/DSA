ef5b2e53c9e9f6fca99ef3879314dd53a6523a8e
pavan <pavangurram56@gmail.com>
2021-12-29 16:35:03 +0530

    learned some more String methods

--- Data Structures and Modern Operators/Strings.js
+++ Data Structures and Modern Operators/Strings.js
@@ -117,3 +117,126 @@ checkBaggage('I have a laptop ,some Food and a pocket Knife', 'sai');
 checkBaggage('socks and Camera', 'pavan');
 checkBaggage('Got some snacks and a gun for protection', 'rahul');
 /************************************************************** */
+///split() AND join()
+
+console.log('a+very+nice+string'.split('+'));
+console.log('pavan kumar'.split(' '));
+const [firstName, lastName] = 'Pavan Kumar'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+//function to convert any string to capitalize string
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.slice(0, 1).toUpperCase() + n.slice(1).toLowerCase()); //another approach
+  }
+  console.log('Mr. ' + namesUpper.join(' ')); //joining the array
+};
+capitalizeName('pAVan kUmAR');
+capitalizeName('saI kumAR is liViNG in cAnAda');
+capitalizeName('posted an announcement');
+
+//padding
+//The padStart() method pads the current string with another string (multiple times, if needed) until the resulting string reaches the given length. The padding is applied from the start of the current string.
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+'));
+console.log('pavan'.padStart(20, '+'));
+///important functionality
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(2324334353548648));
+
+//repeat
+const message2 = 'Bad weather.... all departures Delayed...';
+console.log(message2.repeat(5));
+const planeInLine = function (n) {
+  console.log(`there are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+planeInLine(3);
+planeInLine(5);
+planeInLine(2);
+planeInLine(1);
+
+//
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const button = document.querySelector('button');
+button.addEventListener('click', msg);
+function msg() {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, word] of rows.entries()) {
+    const [first, second] = word.toLowerCase().trim().split('_');
+    const outPut = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${outPut.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+}
+
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split('+')) {
+  const [status, from, to, time] = flight.split(';');
+
+  const res = ` ${status.startsWith('_Delayed') ? '🔴' : ''}${status
+    .slice(1)
+    .split('_')
+    .join(' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45);
+
+  // const answer = res.includes('Delayed') ? `${res}` : `${res}`; //optimized above
+  console.log(res);
+}
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
