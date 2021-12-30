77c6fc6051560c1fdcef76550e11af0234c85ae2
pavan <pavangurram56@gmail.com>
2021-12-30 14:46:34 +0530

    learned more about functionsd

--- Data Structures and Modern Operators/functions.js
+++ Data Structures and Modern Operators/functions.js
@@ -113,5 +113,133 @@ console.log(eurowings);
 book.call(lufthansa, 234, 'sramik');
 console.log(lufthansa);
 book.call(indgo, 34, 'rahul');
-console.log(typeof book);
-console.log(typeof lufthansa.bookings);
+
+//apply method
+const flightData = [583, 'george cooper'];
+book.apply(indgo, flightData); //apply method accepts 2 parameters first one is object and second is array of arguments
+//main diff between call and apply is ..in apply we need pass aruguments in an array
+
+book.call(eurowings, ...flightData); //used call method as apply by passing arguments in an array
+
+//bind() method
+
+const bookEW = book.bind(eurowings); //THE bind() method create a new function that, when called, has its this keyword set to the provided value
+const bookLH = book.bind(lufthansa);
+const bookIND = book.bind(indgo);
+bookEW(12, 'MONIKA');
+bookIND(23, 'kalyan');
+
+const bookIND323 = book.bind(indgo, 323); //here we already attached the flightNum
+bookIND323('lucky'); //only name is enough because we already set the flightNum in bind() method
+
+//with event listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this); //here this keyword is binds to the dom elements because we called the function using event listener
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); //to bind the this keyword to  its object we used the bind() method
+
+//partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, undefined, 100); //here we set the rate and we mentioned null because there is no object for this keyword to points
+console.log(addVAT(0.23));
+console.log(addVAT);
+
+//returning function
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVat2 = addTaxRate(0.23);
+console.log(addVat2(100));
+
+//
+console.log('<<<<<<<<<<----coding challenge------>>>>');
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+const poll = {
+  question: 'What ia your favourite programming language?',
+  options: ['0 : JavaScript', '1: python', '2: rust', '3:c++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const ans = Number(
+      prompt(
+        `${this.question} \n ${this.options.join(
+          '\n'
+        )} \n (write option number)`
+      )
+    );
+    //register the answer
+    typeof ans === 'number' && ans < this.answers.length && this.answers[ans]++; //here we used short circuiting // and increased the answers array index by 1
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type == 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll)); //here we used bind() method because if we didnt used bind() method then this keyword in function points to dom element
+poll.displayResults.call({ answers: [4, 3, 4, 5] });
+poll.displayResults.call({ answers: [4, 3, 4, 5] }, 'string');
+
+//immediately invoked function expression
+
+const runOnce = function () {
+  console.log('this will never run again');
+};
+runOnce();
+
+(function () {
+  //this funnction will invoke without function call >>immediately innvoked function expression
+  console.log('this will never run again');
+})();
+
+(() => console.log('this function invoked without function call'))();
