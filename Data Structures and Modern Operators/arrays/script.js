5985ad67662d947a34531a28b7b98f1cc49a17be
pavan <pavangurram56@gmail.com>
2022-01-02 23:26:26 +0530

    arrays completed

--- Data Structures and Modern Operators/arrays/script.js
+++ Data Structures and Modern Operators/arrays/script.js
@@ -70,9 +70,10 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements; //here we used slice() to make copy of the movements because sort() method mutates the original array
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = ` <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
@@ -104,7 +105,6 @@ const calcDisplaySummary = function (acc) {
     .map(mov => (mov * acc.interestRate) / 100)
     .filter(mov => mov >= 1)
     .reduce((acc, cur) => acc + cur, 0);
-  console.log(interest);
   labelSumInterest.textContent = `${Math.trunc(interest)}₹`;
 };
 
@@ -212,6 +212,14 @@ btnClose.addEventListener('click', function (e) {
   } else {
     console.log('invalid credentials');
   }
+});
+
+//handling sort button
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted); //imp :: toggle of sort button
+  sorted = !sorted;
 });
 
 //computing usernames
@@ -480,6 +488,38 @@ movements.sort((a, b) => a - b); //if a=10,b=20 a-b =>10-20=-10 returns -10 it i
 movements.sort((a, b) => b - a);
 console.log(movements); //op: [-650, -400, -130, 70, 200, 450, 1300, 3000]
 
+//array constructor\
+console.log('<<<<<array constructor>>>>>');
+
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7); //when we give only one value then it takes as array length and it creates empty array with length 7
+
+//empty array + fill method
+// x.fill(1); //OP:[1, 1, 1, 1, 1, 1, 1]
+x.fill(1, 3, 5); //op: [empty × 3, 1, 1, empty × 2] //first parameter = value to be filled , 2nd parameter is starting index and 3rd parameter is last inndex
+console.log(x);
+
+//Array,from
+const y = Array.from({ length: 7 }, () => 1); //op:[1, 1, 1, 1, 1, 1, 1]
+console.log(y);
+
+const z = Array.from({ length: 7 }, (cur, i) => i + 1); //op: [1, 2, 3, 4, 5, 6, 7]
+console.log(z);
+
+// const movementsUI = Array.from();
+
+// array methods practice
+//2.
+console.log('<<<<<<array method practice');
+const bankDepositsum = accounts
+  .map(acc => acc.movements)
+  .flat() //flat() is used to join the arrays we  can  also use flatMap()
+  .filter(mov => mov > 0)
+  .reduce((acc, cur) => acc + cur, 0);
+console.log('total bank deposits = ' + bankDepositsum);
+//2. how many deposits are in bank atleast 1000
+
 // Coding Challenge #1
 
 /* 
@@ -567,3 +607,47 @@ const calcAverageHumanAge = ages => {
   console.log('AVG AGE = ' + avgHumanAge);
 };
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
+*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+console.log(dogs[0]);
+dogs.forEach(dog => (dog.recommendedFood = dog.weight ** 0.75 * 28));
+console.log(dogs[0]);
+
+dogs.forEach(dog => {
+  dog;
+});
